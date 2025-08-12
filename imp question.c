2bd9ea4b0695628b5598970df3080a5de744cfd1
mann2007-ptl <mann.patel.cg@gmail.com>
2025-08-12 16:06:04 +0530

    eight commit

---
+++ imp question.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==0)
+    {printf("ZERO");}
+    else if(a==1)
+    {printf("one");}
+    else if(a==2)
+    {printf("TWO");}
+    else if(a==3)
+    {printf("THREE");}
+    else if(a==4)
+    {printf("FOUR");}
+    else if(a==5)
+    {printf("FIVE");}
+    else if(a==6)
+    {printf("SIX");}
+    else if(a==7)
+    {printf("SEVEN");}
+    else if(a==8)
+    {printf("EIGHT");}
+    else if(a==9)
+    {printf("NINE");}
+    else{printf("not in the range");}
+ return(0);
+
+}
